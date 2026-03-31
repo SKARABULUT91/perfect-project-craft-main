713cf01ae9469ee499fd1ec4352cd3248a069297
SKARABULUT91 <soner91ksk@gmail.com>
2026-03-31 03:17:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/',
   server: {
     port: 3000,
     host: '0.0.0.0',
@@ -12,5 +13,9 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     }
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
   }
 });
